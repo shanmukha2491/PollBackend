5db8312df567ed0ac6d1c3705623086dcd43523f
Shanmukha Moka <shanmukhamoka@Shanmukhas-MacBook-Pro.local>
2025-01-16 12:40:24 +0530

    Fixed Cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,8 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://poll-client-arsovy5jp-shanmukhas-projects-e6e25f49.vercel.app/",
+      "https://poll-client.vercel.app/",
+
     ],
     methods: ["GET", "POST"],
   },
